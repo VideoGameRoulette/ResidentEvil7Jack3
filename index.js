02df0bc8d3a182263ebe275efb0e264b41dcb47f
VideoGameRoulette <videogameroulette@yahoo.ca>
2021-03-30 07:56:12 -0400

    added jack hp

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ window.onload = function () {
 	setInterval(getData, POLLING_RATE);
 };
 
-var Asc = function (a, b) {
-	if (a > b) return +1;
-	if (a < b) return -1;
-	return 0;
-};
-
 var Desc = function (a, b) {
 	if (a > b) return -1;
 	if (a < b) return +1;
@@ -40,23 +34,32 @@ function getData() {
 function appendData(data) {
 	var mainContainer = document.getElementById("srtQueryData");
 	mainContainer.innerHTML = "";
-	var currentMap = data.MapName.split('\0');
 
-	if (currentMap[0].includes("Ship3FInfirmaryPast") && PreviousMap != currentMap[0])
-	{
-		console.log("New Run Started Reseting Boss Values...");
-		InitBosses();
-	}
+	mainContainer.innerHTML += `
+	<div class="tag">
+		<i class="fas fa-skull"></i>
+	</div>
+	<div id="valueBoss"><font size="4" color="#ff0000">0</font></div>`
 
-	data.JackEyeHealth.sort(function (a, b) { return Desc(a.CurrentHP, b.CurrentHP); }).forEach(function (item, index) {
-		if (item.IsAlive) {
-			mainContainer.innerHTML += `
-				<div class="tag">
-					<i class="fas fa-eye"></i>
-				</div>
-				<div id="value"><font size="4" color="#ff0000">${item.CurrentHP}</font></div>`;
+	var BossHP = document.getElementById('valueBoss');
+
+	var filterdEnemies = data.EnemyHealth.filter(m => { return m.MaximumHP == 30000 });
+	//console.log("Filtered Enemies", filterdEnemies);
+	filterdEnemies.sort(function (a, b) { return Asc(a.Percentage, b.Percentage) || Desc(a.CurrentHP, b.CurrentHP); }).forEach(function (item, index, arr) {
+		if (item.IsAlive && index < 1) {
+			BossHP.innerHTML = `<font size="4" color="#ff0000">${item.CurrentHP}</font>`;
 		}
 	});
+
+	if (data.MapName.includes("Boss2F") || data.MapName.includes("Boss1F"))
+	{
+		console.log("Jack 3 Fight Started...");
+		data.JackEyeHealth.sort(function (a, b) { return Desc(a.CurrentHP, b.CurrentHP); }).forEach(function (item, index) {
+			if (item.IsAlive) {
+				mainContainer.innerHTML += `<div class="tag"><i class="fas fa-eye"></i></div><div id="value"><font size="4" color="#ff0000">${item.CurrentHP}</font></div>`;
+			}
+		});
+	}
 }
 
 function clearData() {
